2bfe8eb97058568d6be73417a64096a28044090c
Pedro <tsid.pedro.garrido@gmail.com>
2025-08-19 16:41:18 +0000

    path episodio en singular

--- js/blog.js
+++ js/blog.js
@@ -74,7 +74,7 @@ function renderPosts() {
                <span class="post-duration">${post.duration || ''} min</span>
             </div>
             <p class="post-snippet">${truncateText(post.contentSnippet, 270)}</p>
-            <a href="/episodios/${slug}" 
+            <a href="/episodio/${slug}" 
                class="post-button" 
                title='${post.title ? post.title + " - Escuchar episodio" : "Escuchar episodio"}'>
                ESCUCHAR AHORA
