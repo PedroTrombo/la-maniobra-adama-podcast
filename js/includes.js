dd4d3e9ad8d885ff4d4af4ea5496cbc90f24af62
Pedro <tsid.pedro.garrido@gmail.com>
2025-08-20 11:55:47 +0000

    update nav

--- js/includes.js
+++ js/includes.js
@@ -1,22 +1,26 @@
 async function loadIncludes() {
+  try {
+    // Nav
+    const nav = await fetch('includes/nav.html').then(r => r.text());
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.innerHTML = nav;
 
+    const toggle = navMenu.querySelector(".menu-toggle");
+    const navLinks = navMenu.querySelector(".nav-links");
+    if (toggle && navLinks) {
+      toggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+        toggle.classList.toggle("open"); // útil si quieres cambiar a "X"
+      });
+    }
 
-  // Nav
-  const nav = await fetch('includes/nav.html').then(r => r.text());
-  const navMenu = document.getElementById('nav-menu');
-  navMenu.innerHTML = nav;
+    // Footer
+    const footer = await fetch('includes/footer.html').then(r => r.text());
+    document.getElementById('footer').innerHTML = footer;
 
-  const toggle = navMenu.querySelector(".menu-toggle");
-  const navLinks = navMenu.querySelector(".nav-links");
-  if (toggle && navLinks) {
-    toggle.addEventListener("click", () => {
-      navLinks.classList.toggle("active");
-    });
+  } catch (error) {
+    console.error("Error cargando includes:", error);
   }
-
-  // Footer
-  const footer = await fetch('includes/footer.html').then(r => r.text());
-  document.getElementById('footer').innerHTML = footer;
 }
 
 loadIncludes();
